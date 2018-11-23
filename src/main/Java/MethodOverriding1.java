72b0dce96587eed87c4ffdf08959d50083693ac6
Sushma Baliza <sushma.baliza23@gmail.com>
2018-11-23 07:38:27 +0530

    Method Overriding Programs

---
+++ src/main/Java/MethodOverriding1.java
@@ -0,0 +1,43 @@
+package main.Java;
+
+import java.util.*;
+
+public class MethodOverriding1
+{
+    public static void main(String[] args)
+    {
+        Sports s=new Sports();
+        Soccer so=new Soccer();
+        System.out.println(s.getName());
+        s.getNumberOfTeamMembers();
+        System.out.println(so.getName());
+        so.getNumberOfTeamMembers();
+
+    }
+}
+class Sports{
+    String getName()
+    {
+        return "Generic Sports";
+    }
+    void getNumberOfTeamMembers()
+    {
+        System.out.println("Each team has n players in "+getName());
+
+    }
+}
+class Soccer extends Sports
+{
+   @Override
+    String getName()
+    {
+        return "Soccer Class";
+    }
+    @Override
+    void getNumberOfTeamMembers()
+    {
+        System.out.println("Each team has 11 players in "+getName());
+    }
+
+
+}
